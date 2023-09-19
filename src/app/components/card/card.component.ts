7baa4ac848508acd9bd8821d616cc73b1d510fb5
Ariela88 <135212709+Ariela88@users.noreply.github.com>
2023-09-19 09:43:46 +0200

    up

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Post } from 'src/app/model/post';
+import { StorageService } from 'src/app/services/storage.service';
 
 @Component({
   selector: 'app-card',
@@ -6,6 +8,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./card.component.scss']
 })
 export class CardComponent {
+
+
+  @Input() postData?:Post;
+
+
+  constructor( public storage:StorageService){}
+
+
+
+
+
+
+
 
 
 }
--- src/app/components/list/list.component.ts
+++ src/app/components/list/list.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Post } from 'src/app/model/post';
 import { RedditService } from 'src/app/services/reddit.service';
-import { StorageService } from 'src/app/services/storage.service';
+
 
 @Component({
   selector: 'app-list',
@@ -11,7 +11,7 @@ import { StorageService } from 'src/app/services/storage.service';
 export class ListComponent implements OnInit {
   posts: Post[] = [];
 
-  constructor(private reddit: RedditService, public storage:StorageService) {}
+  constructor(private reddit: RedditService) {}
 
   ngOnInit(): void {
     this.reddit.getRedditPosts().subscribe(childrenData => this.posts = childrenData);
